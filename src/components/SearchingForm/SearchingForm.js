aaad7e3c2558bb9c71a155c5318abcfd9fab1c89
aleksandra-lisek <olalisek.al@gmail.com>
2020-02-29 18:15:32 +0100

    basic searching form

--- src/components/SearchingForm/SearchingForm.js
+++ src/components/SearchingForm/SearchingForm.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import propTypes from 'prop-types';
 // import styled from 'styled-components';
 
 function SearchingForm(props){
-    return <div>
-           
-        </div>
+
+    const [city, setCity] = useState('');
+    
+    const handleChange = (event) => {
+        setCity(event.target.value);
+      }
+
+    const handleSubmit = (event) => {
+        alert('Podano następujące miasto: ' + city);
+        event.preventDefault();
+    }
+
+      console.log(city);
+    return <form  onSubmit={handleSubmit}>
+                <label>
+                    Wpisz miasto:
+                    <input
+                        data-test='component-input'
+                        type='text'
+                        value={city}
+                        onChange={handleChange}
+                    >
+                    </input>
+                </label>
+               <button
+                 data-test='component-button'
+                
+               >
+                   Szukaj
+               </button>
+           </form>
 }
 
 export default SearchingForm;
