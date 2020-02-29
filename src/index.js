8860930b25b3582706cd0f864f79bec0be35e707
aleksandra-lisek <olalisek.al@gmail.com>
2020-02-29 19:02:23 +0100

    adding redux to project

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'react-promise';
+
+const storeWithMiddleware = applyMiddleware(promiseMiddleware)(createStore)
+
+ReactDOM.render(
+<Provider store={storeWithMiddleware(reducers)}>
+    <App />
+</Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import searchingForm from './searchingForm_reducer'
+
+const rootReducer = combineReducers({
+    searchingForm,
+})
+
+export default rootReducer
