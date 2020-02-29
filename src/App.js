e367123522f2271af3ee53a4269bba0e945e8c67
aleksandra-lisek <olalisek.al@gmail.com>
2020-02-29 16:49:07 +0100

    enzyme installed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import SearchingForm from './components/SearchingForm/SearchingForm'
+
 function App() {
   return (
     <div className="App">
+      <SearchingForm/>
     </div>
   );
 }
---
+++ src/components/SearchingForm/SearchingForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import propTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function SearchingForm(props){
+    return <p>Miasto</p>
+}
+
+export default SearchingForm
